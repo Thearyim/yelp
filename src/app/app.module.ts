b8de29cef876fafeca3a82db3fcfad3619b85b6e
ThearyIm <thearyim@gmail.com>
2019-04-05 14:46:23 -0700

    generate business-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { BusinessListComponent } from './business-list/business-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent,
+    BusinessListComponent
   ],
   imports: [
     BrowserModule
